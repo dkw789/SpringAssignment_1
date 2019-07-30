1ffb797f7aab43f041c21203c74dfff3c1d967a1
donny <dkw789@gmail.com>
2019-07-30 16:53:51 -0400

    added integration test

--- src/main/java/com/example/mydemo/model/Person.java
+++ src/main/java/com/example/mydemo/model/Person.java
@@ -45,5 +45,4 @@ public class Person {
         this.job = job;
     }
 
-
 }
--- src/test/java/com/example/mydemo/PersonControllerTest.java
+++ src/test/java/com/example/mydemo/PersonControllerTest.java
@@ -11,21 +11,29 @@ import org.mockito.MockitoAnnotations;
 
 import java.util.Optional;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
-
 public class PersonControllerTest {
     @InjectMocks
-    private PersonController pc;
+    private PersonController personController;
 
     @Mock
     private PersonRepository personRepository;
+
 
     @Before
     public void init() {
         MockitoAnnotations.initMocks(this);
     }
+
+    public void setUp() {
+//        mock = MockMvcBuilders.webAppContextSetup(we).build();
+
+    }
+
 
     @Test
     public void testPersonGet() {
@@ -33,27 +41,37 @@ public class PersonControllerTest {
         p1.setId(1L);
         p1.setFirstName("first");
         p1.setLastName("last");
-        when(personRepository.getOne(1L)).thenReturn(p1);
-        Optional<Person> p2 = pc.get(1L);
+        when(personRepository.findById(1L)).thenReturn(Optional.of(p1));
+
+        Optional<Person> p2;
+        p2 = personController.get(1L);
 //        verify(personRepository).getOne(1L);
         p2.ifPresent(person -> assertEquals(1L, person.getId().longValue()));
+        p2.ifPresent(person -> assertThat(p2.get(), is(1L)));
 
-//  below needs :
-//  import static org.hamcrest.Matchers.*;
-//        assertThat(p2.get(id), is(1L));
+    }
+
+//    @Test
+//    public void testPersonCreate() {
+//        //        p1.setId(1L);
+//        Person p1 = personController.create(new Person(1L, "Sunil", "Kari", "1050 cresent ave", "416-345-4882", "sunil.kari@loblaws.ca", "Architech"));
+//        when(personRepository.findById(1L)).thenReturn(Optional.of(p1));
+//        Optional<Person> p2 = personController.get(1L);
+//        verify(personRepository).findById(1L);
+//        p2.ifPresent(person -> assertEquals("Sunil", person.getFirstName()));
+//    }
+
+
+    @Test
+    public void list() {
 
     }
 
     @Test
-    public void testPersonCreate() {
-        Person p1 = new Person();
-        p1.setId(1L);
-        p1.setFirstName("first");
-        p1.setLastName("last");
-        when(personRepository.getOne(1L)).thenReturn(p1);
-        Optional<Person> p2 = pc.get(1L);
-//        verify(personRepository).getOne(1L);
-        p2.ifPresent(person -> assertEquals("first", person.getFirstName()));
+    public void update() {
     }
 
+    @Test
+    public void delete() {
+    }
 }
