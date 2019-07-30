03edffb961bc8d7bd7305f25c56604d3f3c6aac3
donny <dkw789@gmail.com>
2019-07-30 17:51:26 -0400

    added integration test for Repository

---
+++ src/test/java/com/example/mydemo/PersonRepositoryIntegrationTest.java
@@ -0,0 +1,32 @@
+package com.example.mydemo;
+
+import com.example.mydemo.model.Person;
+import com.example.mydemo.repository.PersonRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.is;
+
+//@RunWith(SpringJUnit4ClassRunner.class)
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class PersonRepositoryIntegrationTest {
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Test
+    public void testFindAll() {
+        List<Person> people = personRepository.findAll();
+        assertThat(people.size(), is(greaterThanOrEqualTo(0)));
+    }
+
+}
